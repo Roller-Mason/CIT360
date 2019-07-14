0692eb81e1f23113605785a4a898a0f6c7afc3b7
Mason <Mason@192.168.86.90>
2019-07-14 13:27:04 -0500

    Finishing up JUnit tests

--- Familiarity/test/familiarity/JavaCollectionsIT.java
+++ Familiarity/test/familiarity/JavaCollectionsIT.java
@@ -5,12 +5,18 @@
  */
 package familiarity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeSet;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  *
@@ -38,25 +44,28 @@ public class JavaCollectionsIT {
     }
 
     /**
-     * Test of run method, of class JavaCollections.
-     */
-    @Test
-    public void testRun() {
-        System.out.println("run");
-        JavaCollections.run();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of arrayListExample method, of class JavaCollections.
      */
     @Test
     public void testArrayListExample() {
-        System.out.println("arrayListExample");
-        JavaCollections.arrayListExample();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        //make a new array list
+        ArrayList arr = new ArrayList();
+        
+        //add to the list
+        arr.add("First element");
+        arr.add("Second element");
+        arr.add("First element"); //duplicate
+        
+        //remove from the list
+        arr.remove(0);
+        
+        //make sure the array isn't null
+        assertNotNull(arr);
+        //make sure the array is what we think it is
+        assertArrayEquals(new String[]{"Second element", "First element"}, ArrayUtils.toStringArray(arr.toArray()));
+        //make sure the first element does not equal "First Element" since we removed it
+        assertFalse(arr.get(0).equals("First Element"));
     }
 
     /**
@@ -64,10 +73,44 @@ public class JavaCollectionsIT {
      */
     @Test
     public void testTreeSetExample() {
-        System.out.println("treeSetExample");
-        JavaCollections.treeSetExample();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        //make a new tree set
+        TreeSet tree = new TreeSet();
+        
+        //add to the set
+        tree.add(2);
+        tree.add(50);
+        tree.add(1);
+        tree.add(2); //duplicate 
+        
+        //clear the list
+        tree.clear();
+        
+        //make sure that after clearing the treeset that it equals an empty set
+        assertEquals(tree, new TreeSet());
+        assertThat(tree, is(new TreeSet()));
+        assertSame(tree.contains(50), false);
+    }
+        /**
+     * Test of hashMapExample method, of class JavaCollections.
+     */
+    @Test
+    public void hashMapExample() {
+        //make a new hash map
+        HashMap hash = new HashMap();
+        
+        //Add elements into the hash map (Produce prices)
+        hash.put("Apple", 3.74);
+        hash.put("Banana", 0.68);
+        hash.put("Pear", 1.25);
+        hash.put("Apple", 5.20);
+        
+        //Make sure we don't have any Kiwis
+        assertNull(hash.get("Kiwi"));
+        //Make sure we have Bananas
+        assertTrue(hash.containsKey("Banana"));
+        //since inserting duplicates, the actual value should be 5.2 not the first 3.74
+        assertNotSame(hash.get("Apple"), 3.74);
     }
     
 }
